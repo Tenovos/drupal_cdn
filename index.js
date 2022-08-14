96fb95d3b63ace8db1ef035180c62b17bf640cd0
Syed Raza Khalid <raza.khalid@tenovos.com>
2022-08-14 04:09:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+    console.log( "Custom JavaScript ready!" );
+    window.STORYLINK_PROFILE = "eyJuYW1lIjoiREVNTyAtIEFNV0FZIiwiYXBpVXJsIjoiaHR0cHM6Ly9kZW1vLnNlcnZpY2VzLnRlbm92b3MuY2xvdWQvY29udGVudC1zdG9yZS12MSIsImFwaUtleSI6IndUdjVSM2phMm82c3lHek9mUkJTRmF0aHNOS1FUY2JsNnNQbFIyakciLCJjbGllbnRJZCI6IjVkNjRnYTNsaWQ2azU2dGZrY3ZqOHBrMWhsIiwidXNlclBvb2xJZCI6InVzLWVhc3QtMV9pMzkxNUlwNk0iLCJjb2duaXRvVXJsIjoiaHR0cHM6Ly90ZW5vdm9zLWRlbW8uYXV0aC51cy1lYXN0LTEuYW1hem9uY29nbml0by5jb20iLCJjb2duaXRvUHJvdmlkZXJOYW1lIjoiRGVtby1Hb29nbGUtT0lEQyIsImNvbnRlbnRVcmwiOiJodHRwczovL2RlbW8uY29udGVudC50ZW5vdm9zLmlvL3dlYi9icmFuZGluZy9hbXdheSIsImFjY291bnRUeXBlIjoibG9jYWxMb2dpbiIsInVzZXJuYW1lIjoiYW13YXkyMDIwLWFkbWluIiwicGFzc3dvcmQiOiJBbXdheTIwMjAxMjMkIiwiaXNOb25FZGl0YWJsZSI6dHJ1ZX0="
+    window.STORYLINK_CONFIG_NAME = "DEMO-AMWAY-WITH-PASS"
+    window.STORYLINK_CUSTOM_CONFIG_URL = 'https://extensions.tenovos.io/storylink/aem/custom-configs/demo-amway-aem-config.js'
+    // window.ENABLE_UPLOAD = true
+    console.log(drupalSettings.remote_image); // value
+
+
+});
